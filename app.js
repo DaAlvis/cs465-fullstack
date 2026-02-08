8bd95b01b0727e8f86e3aa18261061460015599b
Danielle Alvis <danielle.alvis@gmail.com>
2026-02-08 15:12:35 -0800

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
 
+// Define routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
+// Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -23,9 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
